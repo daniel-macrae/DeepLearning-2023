68063ed58b2861efe99db3f9e6699b66a38db183
unknown <juanluislopez24@gmail.com>
2023-04-10 09:38:49 +0200

    save model and losses

--- testHabrok.py
+++ testHabrok.py
@@ -60,13 +60,111 @@ test_loader = DataLoader(test_dataset, batch_size=batch_size, collate_fn=collate
 
 
 # these need changing, probably
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-# Decay LR by a factor of 0.1 every 7 epochs
-scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
-num_epochs = 20
 
-def train_model(model, criterion, optimizer, scheduler=None, num_epochs=25):
+
+def get_args_parser(add_help=True):
+    import argparse
+
+    parser = argparse.ArgumentParser(description="PyTorch Detection Training", add_help=add_help)
+
+    parser.add_argument("--data-path", default="/Data", type=str, help="dataset path")
+    # parser.add_argument("--dataset", default="coco", type=str, help="dataset name")
+    # parser.add_argument("--model", default="maskrcnn_resnet50_fpn", type=str, help="model name")
+    # parser.add_argument("--device", default="cuda", type=str, help="device (Use cuda or cpu Default: cuda)")
+    parser.add_argument(
+        "-b", "--batch-size", default=32, type=int, help="images per gpu, the total batch size is $NGPU x batch_size"
+    )
+    parser.add_argument("--epochs", default=20, type=int, metavar="N", help="number of total epochs to run")
+    # parser.add_argument(
+    #     "-j", "--workers", default=4, type=int, metavar="N", help="number of data loading workers (default: 4)"
+    # )
+    parser.add_argument("--opt", default="sgd", type=str, help="optimizer")
+    parser.add_argument(
+        "--lr",
+        default=0.01,
+        type=float,
+        help="initial learning rate, 0.02 is the default value for training",
+    )
+    parser.add_argument("--momentum", default=0.9, type=float, metavar="M", help="momentum")
+    parser.add_argument(
+        "--wd",
+        "--weight-decay",
+        default=1e-4,
+        type=float,
+        metavar="W",
+        help="weight decay (default: 1e-4)",
+        dest="weight_decay",
+    )
+    # parser.add_argument(
+    #     "--norm-weight-decay",
+    #     default=None,
+    #     type=float,
+    #     help="weight decay for Normalization layers (default: None, same value as --wd)",
+    # )
+    # parser.add_argument(
+    #     "--lr-scheduler", default="multisteplr", type=str, help="name of lr scheduler (default: multisteplr)"
+    # )
+    parser.add_argument(
+        "--lr-step-size", default=8, type=int, help="decrease lr every step-size epochs (multisteplr scheduler only)"
+    )
+    parser.add_argument(
+        "--lr-steps",
+        default=[16, 22],
+        nargs="+",
+        type=int,
+        help="decrease lr every step-size epochs (multisteplr scheduler only)",
+    )
+    parser.add_argument(
+        "--lr-gamma", default=0.1, type=float, help="decrease lr by a factor of lr-gamma"
+    )
+    # parser.add_argument("--print-freq", default=20, type=int, help="print frequency")
+    parser.add_argument("--output-dir", default=".", type=str, help="path to save outputs")
+    # parser.add_argument("--resume", default="", type=str, help="path of checkpoint")
+    # parser.add_argument("--start_epoch", default=0, type=int, help="start epoch")
+    # parser.add_argument("--aspect-ratio-group-factor", default=3, type=int)
+    # parser.add_argument("--rpn-score-thresh", default=None, type=float, help="rpn score threshold for faster-rcnn")
+    # parser.add_argument(
+    #     "--trainable-backbone-layers", default=None, type=int, help="number of trainable layers of backbone"
+    # )
+    # parser.add_argument(
+    #     "--data-augmentation", default="hflip", type=str, help="data augmentation policy (default: hflip)"
+    # )
+    # parser.add_argument(
+    #     "--sync-bn",
+    #     dest="sync_bn",
+    #     help="Use sync batch norm",
+    #     action="store_true",
+    # )
+    # parser.add_argument(
+    #     "--test-only",
+    #     dest="test_only",
+    #     help="Only test the model",
+    #     action="store_true",
+    # )
+
+    # parser.add_argument(
+    #     "--use-deterministic-algorithms", action="store_true", help="Forces the use of deterministic algorithms only."
+    # )
+
+    # distributed training parameters
+    # parser.add_argument("--world-size", default=1, type=int, help="number of distributed processes")
+    # parser.add_argument("--dist-url", default="env://", type=str, help="url used to set up distributed training")
+    # parser.add_argument("--weights", default=None, type=str, help="the weights enum name to load")
+    # parser.add_argument("--weights-backbone", default=None, type=str, help="the backbone weights enum name to load")
+
+    # Mixed precision training parameters
+    # parser.add_argument("--amp", action="store_true", help="Use torch.cuda.amp for mixed precision training")
+
+    # Use CopyPaste augmentation training parameter
+    # parser.add_argument(
+    #     "--use-copypaste",
+    #     action="store_true",
+    #     help="Use CopyPaste data augmentation. Works only with data-augmentation='lsj'.",
+    # )
+
+    return parser
+
+def train_model(model, criterion, optimizer, args, scheduler=None, num_epochs=25):
     since = time.time()
     best_model_wts = copy.deepcopy(model.state_dict())
     best_acc = 0.0
@@ -148,17 +246,54 @@ def train_model(model, criterion, optimizer, scheduler=None, num_epochs=25):
                 print(f'Best loss value epoch {epoch}')
                 best_loss = epoch_loss
                 best_model_wts = copy.deepcopy(model.state_dict())
+                checkpoint = {
+                    "model": best_model_wts,
+                    "optimizer": optimizer.state_dict(),
+                    "args": args,
+                    "epoch": epoch,
+                }
+                torch.save(checkpoint, os.path.join(args.output_dir, f"model_{args.opt.lower()}_{epoch}.pth"))
     
     time_elapsed = time.time() - since
     print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
     print(f'Best loss Acc: {best_loss:4f}')
-    # load best model weights
-    # model.load_state_dict(best_model_wts)
+    checkpoint = {
+        "model": copy.deepcopy(model.state_dict()),
+        "optimizer": copy.deepcopy(optimizer.state_dict()),
+        "args": args,
+        "epoch": epoch,
+        "losses_train": losses_train,
+        "accuracies_train": accuracies,
+        "losses_val": losses_val,
+        "accuracies_val": accuracies_val
+    }
+    torch.save(checkpoint, os.path.join(args.output_dir, f"model_{args.opt.lower()}_{epoch}_final.pth"))
     return model, losses_train, accuracies, losses_val, accuracies_val
 
 
+def createModel(args):
+    model = torchvision.models.detection.ssdlite320_mobilenet_v3_large(weights=None, num_classes=num_classes, weights_backbone=MobileNet_V3_Large_Weights.DEFAULT)
+    # freeze all layers, except the output heads
+    for param in model.parameters():
+        param.requires_grad = False
+    for param in model.head.parameters():
+        param.requires_grad = True 
+    return model
 
 if __name__ == '__main__':
-    model, losses, accuracies, losses_val, accuracies_val = train_model(model, criterion, optimizer, scheduler, num_epochs=num_epochs)
-    print(losses)
-    print(losses_val)
+    args = get_args_parser().parse_args()
+    model = createModel(args)
+    model.to(device)
+    criterion = nn.CrossEntropyLoss()
+    
+    opt_name = args.opt.lower()
+    if opt_name.startswith("sgd"):
+        optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    elif opt_name == "adam":
+        optimizer = optim.Adam(parameters, lr=args.lr)
+    else:
+        raise RuntimeError(f"Invalid optimizer {args.opt}. Only SGD and Adam are supported.")
+        
+    scheduler = lr_scheduler.StepLR(optimizer, step_size=args.lr_step_size, gamma=args.lr_gamma)
+    num_epochs = args.epochs
+    model, losses, accuracies, losses_val, accuracies_val = train_model(model, criterion, optimizer, args, scheduler, num_epochs=num_epochs)
