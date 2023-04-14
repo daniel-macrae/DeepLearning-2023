23b536fffc1394bd4edacc59a0f4a3d3d0d9930f
daniel-macrae <d.macrae@student.rug.nl>
2023-04-14 08:39:36 +0200

    final model training (?)

--- trainModel.py
+++ trainModel.py
@@ -57,19 +57,9 @@ def get_args_parser(add_help=True):
 
     parser = argparse.ArgumentParser(description="PyTorch Detection Training", add_help=add_help)
 
-    #parser.add_argument("--data-path", default="/Data", type=str, help="dataset path")
-    # parser.add_argument("--dataset", default="coco", type=str, help="dataset name")
-    # parser.add_argument("--model", default="maskrcnn_resnet50_fpn", type=str, help="model name")
-    # parser.add_argument("--device", default="cuda", type=str, help="device (Use cuda or cpu Default: cuda)")
-    
-    #parser.add_argument(
-    #    "-b", "--batch-size", default=32, type=int, help="images per gpu, the total batch size is $NGPU x batch_size"
-    #)
     
     parser.add_argument("--epochs", default=40, type=int, metavar="N", help="number of total epochs to run")
-    # parser.add_argument(
-    #     "-j", "--workers", default=4, type=int, metavar="N", help="number of data loading workers (default: 4)"
-    # )
+    
     parser.add_argument("--opt", default="sgd", type=str, help="optimizer")
     parser.add_argument("--train-backbone", default=False, type=bool, help="optimizer")
     parser.add_argument(
@@ -79,24 +69,7 @@ def get_args_parser(add_help=True):
         help="initial learning rate, 0.02 is the default value for training",
     )
     parser.add_argument("--momentum", default=0.9, type=float, metavar="M", help="momentum")
-    #parser.add_argument(
-    #    "--wd",
-    #    "--weight-decay",
-    #    default=1e-4,
-    #    type=float,
-    #    metavar="W",
-    #    help="weight decay (default: 1e-4)",
-    #    dest="weight_decay",
-    #)
-    # parser.add_argument(
-    #     "--norm-weight-decay",
-    #     default=None,
-    #     type=float,
-    #     help="weight decay for Normalization layers (default: None, same value as --wd)",
-    # )
-    # parser.add_argument(
-    #     "--lr-scheduler", default="multisteplr", type=str, help="name of lr scheduler (default: multisteplr)"
-    # )
+    
     parser.add_argument(
         "--lr-step-size", default=8, type=int, help="decrease lr every step-size epochs (multisteplr scheduler only)"
     )
@@ -110,50 +83,7 @@ def get_args_parser(add_help=True):
     parser.add_argument(
         "--lr-gamma", default=0.1, type=float, help="decrease lr by a factor of lr-gamma"
     )
-    # parser.add_argument("--print-freq", default=20, type=int, help="print frequency")
     parser.add_argument("--output-dir", default="Results", type=str, help="path to save outputs")
-    # parser.add_argument("--resume", default="", type=str, help="path of checkpoint")
-    # parser.add_argument("--start_epoch", default=0, type=int, help="start epoch")
-    # parser.add_argument("--aspect-ratio-group-factor", default=3, type=int)
-    # parser.add_argument("--rpn-score-thresh", default=None, type=float, help="rpn score threshold for faster-rcnn")
-    # parser.add_argument(
-    #     "--trainable-backbone-layers", default=None, type=int, help="number of trainable layers of backbone"
-    # )
-    # parser.add_argument(
-    #     "--data-augmentation", default="hflip", type=str, help="data augmentation policy (default: hflip)"
-    # )
-    # parser.add_argument(
-    #     "--sync-bn",
-    #     dest="sync_bn",
-    #     help="Use sync batch norm",
-    #     action="store_true",
-    # )
-    # parser.add_argument(
-    #     "--test-only",
-    #     dest="test_only",
-    #     help="Only test the model",
-    #     action="store_true",
-    # )
-
-    # parser.add_argument(
-    #     "--use-deterministic-algorithms", action="store_true", help="Forces the use of deterministic algorithms only."
-    # )
-
-    # distributed training parameters
-    # parser.add_argument("--world-size", default=1, type=int, help="number of distributed processes")
-    # parser.add_argument("--dist-url", default="env://", type=str, help="url used to set up distributed training")
-    # parser.add_argument("--weights", default=None, type=str, help="the weights enum name to load")
-    # parser.add_argument("--weights-backbone", default=None, type=str, help="the backbone weights enum name to load")
-
-    # Mixed precision training parameters
-    # parser.add_argument("--amp", action="store_true", help="Use torch.cuda.amp for mixed precision training")
-
-    # Use CopyPaste augmentation training parameter
-    # parser.add_argument(
-    #     "--use-copypaste",
-    #     action="store_true",
-    #     help="Use CopyPaste data augmentation. Works only with data-augmentation='lsj'.",
-    # )
 
     return parser
 
@@ -328,6 +258,7 @@ if __name__ == '__main__':
     model = createModel(args)
     model.to(device)
     criterion = nn.CrossEntropyLoss()
+    print(model)
     
     opt_name = args.opt.lower()
     if opt_name.startswith("sgd"):
--- trainSSD2.sh
+++ trainSSD2.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=testingSSDLite
-#SBATCH --time=5:00:00
+#SBATCH --time=3:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --mem=32000
+#SBATCH --mem=8000
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=d.macrae@student.rug.nl
 module load Miniconda3
 conda create -n SSDLiteEnv python=3.8
 source activate SSDLiteEnv
 pip install -r requirements.txt
-python testHabrok.py --opt "adam" --train-backbone True --epochs 50 --lr 0.001
-python testHabrok.py --train-backbone True --epochs 50 --lr 0.001
+python trainModel.py --train-backbone False --lr 0.0001 --lr-step-size 5
